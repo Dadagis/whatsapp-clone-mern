325050e6c22175b95dd3ee576c970aaf068d53e6
David <davidobob@laposte.net>
2020-09-27 12:20:44 +0200

    added login form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../style/form.css";
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
+      <div className="form">
         <h1>Connexion</h1>
-        <label htmlFor="email">E-mail</label>
-        <input type="text" placeholder="toto@exemple.fr" id="email" />
-        <label htmlFor="password">Mot de passe</label>
-        <input type="text" placeholder="*********" id="password" />
+        <div className="form-inputs">
+          <label htmlFor="email">E-mail</label>
+          <input type="text" placeholder="toto@exemple.fr" id="email" />
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" placeholder="*********" id="password" />
+          <button type="submit">Se connecter</button>
+        </div>
+        <div className="link-div">
+          <Link className="link">Je n'ai pas de compte</Link>
+        </div>
       </div>
     );
   }
