ea3dd7a4efdf7ef68dc226a800a7ff1f19530f4e
David <davidobob@laposte.net>
2020-10-05 10:13:10 +0200

    made chat app a functional component

--- src/components/chatApp.jsx
+++ src/components/chatApp.jsx
@@ -1,16 +1,49 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 import Pusher from "pusher-js";
 import axios from "../axios";
 
-export default class ChatApp extends Component {
-  state = {
-    messages: [],
-    setMessages: [],
-  };
+export default function ChatApp() {
+  // state = {
+  //   messages: [],
+  //   setMessages: [],
+  // };
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   axios
+  //     .get("/api/messages/sync", {
+  //       headers: {
+  //         "x-auth-token":
+  //           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjY3MTRhNGMzM2IwZTBiNjE2YzhjOTQiLCJpYXQiOjE2MDA1OTE0NDF9.DMGE9ZpHvYw2vy2ruR6Zqj_H63OtPmu-usp0n-Pj3Bc",
+  //       },
+  //     })
+  //     .then((response) => {
+  //       this.setState({ setMessages: response.data });
+  //       //   setMessages(response.data);
+  //     });
+  // }
+
+  // componentWillUpdate() {
+  //   const pusher = new Pusher("dbb1d6a74095b5cc4f07", {
+  //     cluster: "eu",
+  //   });
+
+  //   const channel = pusher.subscribe("messages");
+  //   channel.bind("inserted", (data) => {
+  //     this.setState({ setMessages: [...this.state.setMessages, data] });
+  //   });
+
+  //   // à modifier, ne fonctionne que dans un Hook pour le moment
+  //   return () => {
+  //     channel.unbind_all();
+  //     channel.unsubscribe();
+  //   };
+  // }
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
     axios
       .get("/api/messages/sync", {
         headers: {
@@ -19,66 +52,31 @@ export default class ChatApp extends Component {
         },
       })
       .then((response) => {
-        this.setState({ setMessages: response.data });
-        //   setMessages(response.data);
+        setMessages(response.data);
       });
-  }
+  }, []);
 
-  componentWillUpdate() {
+  useEffect(() => {
     const pusher = new Pusher("dbb1d6a74095b5cc4f07", {
       cluster: "eu",
     });
 
     const channel = pusher.subscribe("messages");
     channel.bind("inserted", (data) => {
-      this.setState({ setMessages: [...this.state.setMessages, data] });
+      setMessages([...messages, data]);
     });
 
-    // à modifier, ne fonctionne que dans un Hook pour le moment
     return () => {
       channel.unbind_all();
       channel.unsubscribe();
     };
-  }
+  }, [messages]);
 
-  render() {
-    // const [messages, setMessages] = useState([]);
-
-    // useEffect(() => {
-    //   axios
-    //     .get("/api/messages/sync", {
-    //       headers: {
-    //         "x-auth-token":
-    //           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjY3MTRhNGMzM2IwZTBiNjE2YzhjOTQiLCJpYXQiOjE2MDA1OTE0NDF9.DMGE9ZpHvYw2vy2ruR6Zqj_H63OtPmu-usp0n-Pj3Bc",
-    //       },
-    //     })
-    //     .then((response) => {
-    //       setMessages(response.data);
-    //     });
-    // }, []);
-
-    // useEffect(() => {
-    //   const pusher = new Pusher("dbb1d6a74095b5cc4f07", {
-    //     cluster: "eu",
-    //   });
-
-    //   const channel = pusher.subscribe("messages");
-    //   channel.bind("inserted", (data) => {
-    //     setMessages([...messages, data]);
-    //   });
-
-    //   return () => {
-    //     channel.unbind_all();
-    //     channel.unsubscribe();
-    //   };
-    // }, [messages]);
-
-    // console.log(messages);
-    return (
-      <div className="App-body">
-        <Sidebar />
-        <Chat messages={this.state.setMessages} />
-      </div>
-    );
-  }
+  console.log(messages);
+  return (
+    <div className="App-body">
+      <Sidebar />
+      <Chat messages={messages} />
+    </div>
+  );
 }
