084a349b9d336348a3b1d11f0e3665b95cc41c2d
David <davidobob@laposte.net>
2020-10-05 12:05:31 +0200

    added register form and store the token in local storage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import ChatApp from "./components/chatApp";
 import Login from "./components/login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route path="/" component={ChatApp} />
       </Switch>
     </div>
---
+++ src/components/Register.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/form.css";
+import axios from "../axios";
+import { useState } from "react";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post("/api/users/", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        localStorage.setItem(
+          "whatsApp-token",
+          response.headers["x-auth-token"]
+        );
+      });
+  };
+  const handleChange = ({ currentTarget: input }) => {
+    if (input.id === "name") {
+      setName(input.value);
+    } else if (input.id === "email") {
+      setEmail(input.value);
+    } else {
+      setPassword(input.value);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Inscription</h1>
+      <form>
+        <div className="form-inputs">
+          <label htmlFor="email">Name</label>
+          <input
+            type="text"
+            placeholder="Toto"
+            id="name"
+            value={name}
+            onChange={handleChange}
+          />
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            placeholder="toto@exemple.fr"
+            id="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="password"
+            placeholder="*********"
+            id="password"
+            value={password}
+            onChange={handleChange}
+          />
+          <button onClick={handleClick} type="submit">
+            S'inscrire
+          </button>
+        </div>
+      </form>
+      <div className="link-div">
+        <Link to="/login" className="link">
+          J'ai déja un compte
+        </Link>
+      </div>
+    </div>
+  );
+}
