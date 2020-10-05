d86fd7c272a366e46887324ef62235aa4f126e47
David <davidobob@laposte.net>
2020-10-05 13:33:14 +0200

    login and redirect working

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,18 +1,65 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../style/form.css";
+import instance from "../axios";
 
-export default function Login() {
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await instance
+        .post("/api/auth/login", { email: email, password: password })
+        .then((response) => {
+          localStorage.setItem(
+            "whatsApp-token",
+            response.headers["x-auth-token"]
+          );
+        });
+      props.history.push("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleChange = ({ currentTarget: input }) => {
+    if (input.id === "email") {
+      setEmail(input.value);
+    } else {
+      setPassword(input.value);
+    }
+  };
+
   return (
     <div className="form">
       <h1>Connexion</h1>
-      <div className="form-inputs">
-        <label htmlFor="email">E-mail</label>
-        <input type="text" placeholder="toto@exemple.fr" id="email" />
-        <label htmlFor="password">Mot de passe</label>
-        <input type="password" placeholder="*********" id="password" />
-        <button type="submit">Se connecter</button>
-      </div>
+      <form>
+        <div className="form-inputs">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            placeholder="toto@exemple.fr"
+            id="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="password"
+            placeholder="*********"
+            id="password"
+            value={password}
+            onChange={handleChange}
+          />
+          <button onClick={handleClick} type="submit">
+            Se connecter
+          </button>
+        </div>
+      </form>
       <div className="link-div">
         <Link to="/register" className="link">
           Je n'ai pas de compte
