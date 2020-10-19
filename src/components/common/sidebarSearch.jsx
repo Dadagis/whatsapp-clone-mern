25ae7a847f66e3dc8308a7b7612bb8ed81cac838
David <davidobob@laposte.net>
2020-10-19 12:40:42 +0200

    basic filtering test

--- src/components/common/sidebarSearch.jsx
+++ src/components/common/sidebarSearch.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
 
 export default function SidebarSearch(props) {
+  const [users, setUsers] = useState([]);
+  const [input, setInput] = useState("");
+  const [filtered, setFiltered] = useState([]);
+
+  const jwt = localStorage.getItem("whatsAppToken");
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/users/", {
+        headers: { "x-auth-token": jwt },
+      })
+      .then((response) => {
+        return setUsers(response.data);
+      });
+  }, []);
+
+  const setValue = ({ currentTarget: input }) => {
+    setInput(input.value);
+  };
+
+  useEffect(() => {
+    const filtered = users.filter((user) => user.name.includes(input));
+    return setFiltered(filtered);
+  }, [input]);
+
+  console.log("filtered", filtered);
+
   return (
     <div className="sidebar-search">
       <div className="search-container">
         <SearchIcon className="search-icon" />
-        <input placeholder="Chercher ou démarrer un nouveau chat" type="text" />
+        <input
+          value={input}
+          onChange={setValue}
+          placeholder="Chercher ou démarrer un nouveau chat"
+          type="text"
+        />
+        <ul></ul>
       </div>
     </div>
   );
